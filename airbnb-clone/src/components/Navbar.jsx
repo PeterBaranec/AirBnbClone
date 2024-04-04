460cedd273e69ead4d4f3209d89a585deb6e66fe
Peter Baranec <peter.baranec.devtech@gmail.com>
2024-04-04 11:52:15 +0200

    fix

--- airbnb-clone/src/components/Navbar.jsx
+++ airbnb-clone/src/components/Navbar.jsx
@@ -5,10 +5,10 @@ import photo from "../assets/photo-grid.png"
 function Navbar() {
     return (
         <header>
-            <nav className="nav"><img src={logo} alt="" className="nav-logo"/></nav>
-            <img src={photo} alt="" className="nav-image" />
-            <h1 className="nav-title">Online Experiences</h1>
-            <p className="description">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+            <nav className="nav"><img src={logo} alt="AirBnb logo" className="nav__logo"/></nav>
+            <img src={photo} alt="photos" className="nav__image" />
+            <h1 className="nav__title">Online Experiences</h1>
+            <p className="nav__description">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
         </header>
     )
 }
