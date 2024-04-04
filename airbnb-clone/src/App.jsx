e9120d8044d6ebc84a21252cd72e0ece05fa8f4f
Peter Baranec <peter.baranec.devtech@gmail.com>
2024-04-04 17:14:49 +0200

    props

--- airbnb-clone/src/App.jsx
+++ airbnb-clone/src/App.jsx
@@ -1,17 +1,22 @@
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Card from './components/Card'
-import './App.css'
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <Navbar />
       <Hero />
-      <Card />
+      <Card
+        rating="5"
+        reviewCount="6"
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price="136"
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- airbnb-clone/src/components/Card.jsx
+++ airbnb-clone/src/components/Card.jsx
@@ -1,21 +1,23 @@
-import "../components/Card.css"
-import photo from "../assets/katie-zaferes.png"
-import star from "../assets/star.png"
+import "../components/Card.css";
+import photo from "../assets/katie-zaferes.png";
+import star from "../assets/star.png";
 
-function Card() {
-    return (
-        <div className="card">
-            <img src={photo} alt="" className="card__image"/>
-            <div className="card__stats">
-                <img src={star} alt="" className="card__star"/>
-                <span>5.0</span>
-                <span className="gray">(6)</span>
-                <span className="gray">USA</span>
-            </div>
-            <p className="card__description">Life lessons with Katie Zaferes</p>
-            <p className="card__price"><span className="bold">From $136</span> / person</p>
-        </div>
-    )
+function Card(props) {
+  return (
+    <div className="card">
+      <img src={photo} alt="" className="card__image" />
+      <div className="card__stats">
+        <img src={star} alt="" className="card__star" />
+        <span>{props.rating}</span>
+        <span className="gray">({props.reviewCount}) • </span>
+        <span className="gray">{props.country}</span>
+      </div>
+      <h2 className="card__description">{props.title}</h2>
+      <p className="card__price">
+        <span className="bold">From {props.price}</span> / person
+      </p>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
